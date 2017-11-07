80838173e14d0b9994b82c4f386289173bd41fa2
alexiso <alexiso@umich.edu>
2017-11-07 11:47:38 -0500

    Added color printing and logging

--- rpn.py
+++ rpn.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 import operator 
+import readline
+import logging 
+import sys
+from termcolor import colored, cprint
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 ops = {
@@ -12,27 +19,35 @@ ops = {
 	'//': operator.floordiv,
 }
 
+
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
 		try:
 			token = int(token)
 			stack.append(token)
+			print(token, ' ')
 		except ValueError:
 			arg2 = stack.pop()
 			arg1 = stack.pop()
+			# token = colored(token, on_cyan)
+			cprint(token, 'white', 'on_cyan')
 			function = ops[token]
 			result = function(arg1, arg2)
 			stack.append(result)
 	if len(stack) != 1:
 		raise TypeError("Too many parameters")
-	print(stack)
+	logger.debug(stack)
 	return stack.pop()
 
 def main():
 	while True:
 		result = calculate(input("rpn calc> "))
-		print("Result: ", result)
+		if result < 0:
+			result = "Result: " + str(result)
+			cprint(result, 'red')
+		else:
+			print("Result: ", result)
 
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
 	main()
