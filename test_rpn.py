87fb6e9c8c7684010fd45149056b2312a470e9d5
alexiso <alexiso@umich.edu>
2017-11-07 10:21:51 -0500

    Added mult and divide test

--- test_rpn.py
+++ test_rpn.py
@@ -8,4 +8,10 @@ class TestBasic(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+	def test_multiply(self):
+		result = rpn.calculate("5 3 *")
+		self.assertEqual(15, result)
+	def test_divide(self):
+		result = rpn.calculate("6 3 /")
+		self.assertEqual(2, result)
 
